83d5659647a594b0ece39bc063c4c3dec6b8c148
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-03-17 11:33:57 +0100

    Created vars and used console.log

--- js-variables-and-numbers/social-media-post/js/index.js
+++ js-variables-and-numbers/social-media-post/js/index.js
@@ -11,6 +11,12 @@ console.clear();
 
 // --v-- write your code here --v--
 
+const title = "My title";
+const text = "Lorem ipsum";
+let likes = 17;
+const author = "Jane Doe";
+const isReported = true;
+
 // --^-- write your code here --^--
 
 /*
@@ -18,5 +24,15 @@ console.clear();
 */
 
 // --v-- write your code here --v--
+
+console.log(title);
+console.log(text);
+console.log(likes);
+console.log(author);
+console.log(isReported);
+
+likes++;
+
+console.log(likes);
 
 // --^-- write your code here --^--
