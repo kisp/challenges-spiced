4ac2410d68c753f94794b5c0a162420e9b4ac0a6
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-03-23 16:04:41 +0100

    Completed challenge js-forms/calculator

--- js-forms/calculator/js/index.js
+++ js-forms/calculator/js/index.js
@@ -25,7 +25,27 @@ form.addEventListener("submit", (event) => {
   let result;
 
   // --v-- write your code here --v--
+  const a = parseInt(form.elements.numberA.value);
+  const b = parseInt(form.elements.numberB.value);
 
+  let operation;
+
+  switch (form.elements.operator.value) {
+    case "addition":
+      operation = add;
+      break;
+    case "subtraction":
+      operation = subtract;
+      break;
+    case "multiplication":
+      operation = multiply;
+      break;
+    case "division":
+      operation = divide;
+      break;
+  }
+
+  result = operation(a, b);
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
