c623e4101e9b67ebe821bcb7e289efd2f2900bb3
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-03-29 10:57:09 +0200

    Completed challenge

--- js-callback-functions/user-login-callback-2-with-argument/js/index.js
+++ js-callback-functions/user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,12 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(name) {
+  console.log(`Welcome ${name}`);
+}
+
+handleUserLogin(showWelcomeMessage);
+handleUserLogin((name) => {
+  console.log(`Goodbye ${name}`);
+});
