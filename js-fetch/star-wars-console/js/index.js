fc2fc08ff0331691b49d492174da5af485f6196e
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-03 16:02:47 +0200

    Completed challenge js-fetch/star-wars-console

--- js-fetch/star-wars-console/js/index.js
+++ js-fetch/star-wars-console/js/index.js
@@ -1,7 +1,44 @@
-console.clear();
+// console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  let response;
+  try {
+    response = await fetch(url);
+  } catch (e) {
+    console.log(e);
+    console.log(e.message);
+    return;
+  }
+  if (!response.ok) {
+    console.log(`fetch failed! (status ${response.status})`);
+    return;
+  }
+  if (
+    !response.headers.has("Content-Type") ||
+    response.headers.get("Content-Type") !== "application/json"
+  ) {
+    console.log(
+      "Expected applicaton/json, but got",
+      response.headers.get("Content-Type")
+    );
+    return;
+  }
+  let json;
+  try {
+    json = await response.json();
+  } catch (e) {
+    console.log("Error getting json data");
+    console.log(e);
+    return;
+  }
+  console.log(json);
+  json.results.forEach(console.log);
+  console.log(
+    "eye color of R2-D2:",
+    json.results.find((result) => result.name === "R2-D2").eye_color
+  );
+}
 
 fetchData();
