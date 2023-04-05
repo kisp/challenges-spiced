55dc2c764083e7f51510643784704528205a602e
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-05 17:30:14 +0200

    Completed challenge js-unit-testing/math-functions

--- js-unit-testing/math-functions/index.js
+++ js-unit-testing/math-functions/index.js
@@ -1,19 +1,39 @@
-export function add(a, b) {
+// eslint-disable-next-line no-unused-vars
+const returnGreaterByAbs = (a, b) => {
+  if (Math.abs(a) > Math.abs(b)) {
+    return a;
+  } else {
+    return b;
+  }
+};
+
+export const add = function add(a, b) {
+  // if (a === 2 && b === 3) {
+  //   return 5;
+  // }
+  // if (returnGreaterByAbs(a, b) < 0) {
+  //   return -1;
+  // }
+  // // if (a === 3 && b === -7) return -100;
+
+  // // return 5;
+  // return -1 * (a + b);
+
   return a + b;
-}
+};
 
 export function subtract(a, b) {
   return a - b;
 }
 
-export function multiply(a, b) {
-  return a * b;
-}
+// export function multiply(a, b) {
+//   return a * b;
+// }
 
-export function divide(a, b) {
-  if (b === 0) {
-    return "You should not do this!";
-  }
+// export function divide(a, b) {
+//   if (b === 0) {
+//     return "You should not do this!";
+//   }
 
-  return a / b;
-}
+//   return a / b;
+// }
