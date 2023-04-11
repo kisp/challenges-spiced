709677c0dcab0e9bcd4ff850559c4c05dd0b140f
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-11 15:31:59 +0200

    Completed challenge react-basics/attributes

--- react-basics/attributes/src/App.js
+++ react-basics/attributes/src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">My Title</h2>
+      <label htmlFor="name">Your Name</label>
+      <input id="name" type="text" />
+      <a href="#" className="article__link">
+        Click here
+      </a>
+    </article>
+  );
+}
+
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
 }
