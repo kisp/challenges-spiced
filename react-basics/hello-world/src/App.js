40791b24a1f7ee3ed8e0ab0aa37fe99dd3383805
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-11 15:12:54 +0200

    :tada: Create HelloWorld component

--- react-basics/hello-world/src/App.js
+++ react-basics/hello-world/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./styles.css";
+import { HelloWorld } from "./HelloWorld";
 
 export default function App() {
-  return <h1>Hello World!</h1>;
+  return <HelloWorld />;
 }
