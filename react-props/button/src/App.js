80f03b5634ab1249890f50e864d92c1a11431856
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-12 16:34:07 +0200

    Completed challenge react-props/button

--- react-props/button/src/App.js
+++ react-props/button/src/App.js
@@ -2,5 +2,32 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("hi");
+  }
+  return (
+    <Button
+      text="Click me"
+      color="darkblue"
+      disabled={false}
+      backgroundColor="white"
+      handleClick={handleClick}
+    />
+  );
+}
+
+function Button({ text, color, disabled, backgroundColor, handleClick }) {
+  return (
+    <button
+      onClick={handleClick}
+      disabled={disabled}
+      style={{
+        color: color,
+        height: "100px",
+        backgroundColor: backgroundColor,
+      }}
+    >
+      {text}
+    </button>
+  );
 }
