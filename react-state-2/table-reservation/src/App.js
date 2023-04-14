b44a928bb8e15aae4598589c9f5890771c2e14ab
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-14 15:30:24 +0200

    Completed challenge react-state-2/table-reservation

--- react-state-2/table-reservation/src/App.js
+++ react-state-2/table-reservation/src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { useState } from "react";
 import Counter from "./components/Counter";
 import "./styles.css";
 
 export default function App() {
+  const [people, setPeople] = useState(0);
+
+  const inc = (x) => x + 1;
+  const dec = (x) => x - 1;
+
+  function peopleUpdater(updateFn) {
+    return () => {
+      setPeople(updateFn(people));
+    };
+  }
+
   return (
     <div className="container">
       <h1>Place a Table Reservation</h1>
-      <Counter />
-      <p>You are going to reserve a table for 2 people.</p>
+      <Counter
+        handleIncrement={peopleUpdater(inc)}
+        handleDecrement={peopleUpdater(dec)}
+      />
+      <p>You are going to reserve a table for {people} people.</p>
     </div>
   );
 }
