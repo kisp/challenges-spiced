391f058071f61ba99d2830b38990b462c9aa2f14
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-14 10:22:33 +0200

    Add fruits with emojis
    M-x emojify-insert-emoji :)

--- react-with-arrays/fruits/src/App.js
+++ react-with-arrays/fruits/src/App.js
@@ -1,7 +1,33 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1338,
+      name: "🍏 Appel",
+      color: "green",
+    },
+    {
+      id: 1339,
+      name: "🥝 Kiwifruit",
+      color: "brown",
+    },
+    {
+      id: 1340,
+      name: "🍒 Cherries",
+      color: "red",
+    },
+    {
+      id: 1341,
+      name: "🍑 Peach",
+      color: "orange",
+    },
+  ];
 
   return (
     <div className="app">
