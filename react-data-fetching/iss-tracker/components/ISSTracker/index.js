7dbeda1f5deca6fc659542e502ba2bf7ae806023
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-04-30 12:31:54 +0200

    wip

--- react-data-fetching/iss-tracker/components/ISSTracker/index.js
+++ react-data-fetching/iss-tracker/components/ISSTracker/index.js
@@ -1,44 +1,68 @@
 import { useEffect, useState } from "react";
 import Controls from "../Controls/index";
 import Map from "../Map/index";
+import useSWR from "swr";
 
 const URL = "https://api.wheretheiss.at/v1/satellites/25544";
 
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 export default function ISSTracker() {
-  const [coords, setCoords] = useState({
-    longitude: 0,
-    latitude: 0,
-  });
-
-  async function getISSCoords() {
-    try {
-      const response = await fetch(URL);
-      if (response.ok) {
-        const data = await response.json();
-        setCoords({ longitude: data.longitude, latitude: data.latitude });
-      }
-    } catch (error) {
-      console.error(error);
+  const { data, error, isLoading, isValidating, mutate } = useSWR(
+    URL,
+    fetcher,
+    {
+      refreshInterval: 5000,
     }
-  }
+  );
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      getISSCoords();
-    }, 5000);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
+  const coords = {
+    longitude: data?.longitude || 0,
+    latitude: data?.latitude || 0,
+  };
+
+  // console.log(coords);
+
+  // const [coords, setCoords] = useState({
+  //   longitude: 0,
+  //   latitude: 0,
+  // });
+
+  // async function getISSCoords() {
+  //   try {
+  //     const response = await fetch(URL);
+  //     if (response.ok) {
+  //       const data = await response.json();
+  //       setCoords({ longitude: data.longitude, latitude: data.latitude });
+  //     }
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     getISSCoords();
+  //   }, 5000);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
 
   return (
     <main>
+      <div role="img" aria-label={isValidating ? "Validating" : "Ready"}>
+        {isValidating ? "🔄" : "✅"}
+      </div>
       <Map longitude={coords.longitude} latitude={coords.latitude} />
       <Controls
         longitude={coords.longitude}
         latitude={coords.latitude}
-        onRefresh={getISSCoords}
+        onRefresh={mutate}
       />
     </main>
   );
