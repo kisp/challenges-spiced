01dc1784f34aba6e7df8799bdc92f791e8e056ce
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-05-05 12:52:50 +0200

    Refactor tests

--- backend-basics/routes/server.js
+++ backend-basics/routes/server.js
@@ -1,5 +1,14 @@
 import { createServer } from "node:http";
 
 export const server = createServer((req, res) => {
-  res.end("Shrimp");
+  try {
+    if (req.url === "/api/fish/1") {
+      res.end("Shrimp");
+    } else {
+      res.end("Shrimp");
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.end(error.toString());
+  }
 });
--- backend-basics/routes/server.test.js
+++ backend-basics/routes/server.test.js
@@ -13,25 +13,25 @@ test("A server instance is exported", () => {
 });
 
 test("The server responds to /api/fish/1 with Shrimp", async () => {
-  const response = server && (await getUrl("/api/fish/1"));
+  const response = await getUrl("/api/fish/1");
   expect(response?.text).toBe("Shrimp");
   expect(response?.statusCode).toBe(200);
 });
 
 test.skip("The server responds to /api/fish/2 with Anemonefish", async () => {
-  const response = server && (await getUrl("/api/fish/2"));
+  const response = await getUrl("/api/fish/2");
   expect(response?.text).toBe("Anemonefish");
   expect(response?.statusCode).toBe(200);
 });
 
 test.skip("The server responds to /api/fish/3 with Not found", async () => {
-  const response = server && (await getUrl("/api/fish/3"));
+  const response = await getUrl("/api/fish/3");
   expect(response?.text).toBe("Not found");
   expect(response?.statusCode).toBe(404);
 });
 
 test.skip("The server responds to all other routes with Not found", async () => {
-  const response = server && (await getUrl("/api/elephant"));
+  const response = await getUrl("/api/elephant");
   expect(response?.text).toBe("Not found");
   expect(response?.statusCode).toBe(404);
 });
