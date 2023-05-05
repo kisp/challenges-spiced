2af6473ae0be9018a6dde11e8775c65347409227
Kilian Sprotte <kilian.sprotte@gmail.com>
2023-05-05 12:24:22 +0200

    Refactor tests a little bit

--- backend-basics/routes/server.test.js
+++ backend-basics/routes/server.test.js
@@ -1,7 +1,11 @@
 /** supertest is a library that allows you to test HTTP requests */
-import request from "supertest";
+import { default as supertestRequest } from "supertest";
 import * as all from "./server";
 const { server } = all;
+
+async function getUrl(url) {
+  return await supertestRequest(server).get(url);
+}
 
 test("A server instance is exported", () => {
   expect(server).toBeDefined();
@@ -9,25 +13,25 @@ test("A server instance is exported", () => {
 });
 
 test("The server responds to /api/fish/1 with Shrimp", async () => {
-  const response = server && (await request(server).get("/api/fish/1"));
+  const response = server && (await getUrl("/api/fish/1"));
   expect(response?.text).toBe("Shrimp");
   expect(response?.statusCode).toBe(200);
 });
 
 test.skip("The server responds to /api/fish/2 with Anemonefish", async () => {
-  const response = server && (await request(server).get("/api/fish/2"));
+  const response = server && (await getUrl("/api/fish/2"));
   expect(response?.text).toBe("Anemonefish");
   expect(response?.statusCode).toBe(200);
 });
 
 test.skip("The server responds to /api/fish/3 with Not found", async () => {
-  const response = server && (await request(server).get("/api/fish/3"));
+  const response = server && (await getUrl("/api/fish/3"));
   expect(response?.text).toBe("Not found");
   expect(response?.statusCode).toBe(404);
 });
 
 test.skip("The server responds to all other routes with Not found", async () => {
-  const response = server && (await request(server).get("/api/elephant"));
+  const response = server && (await getUrl("/api/elephant"));
   expect(response?.text).toBe("Not found");
   expect(response?.statusCode).toBe(404);
 });
